289ede0f09bde8a4db61843e39c9a6aa580fa152
Dmitriy Olin <1988navras@gmail.com>
2019-12-19 11:47:18 +0300

    proj start

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+let webpack = require('webpack');
+let HtmlPlugin = require('html-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let rules = require('./webpack.config.rules')();
+let path = require('path');
+
+rules.push({
+    test: /\.css$/,
+    use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader'
+    })
+});
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+    devServer: {
+        overlay: true
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve('dist'),
+    },
+    devtool: 'source-map',
+    module: { rules },
+    plugins: [
+        new ExtractTextPlugin('styles.css'),
+        new HtmlPlugin({
+            title: 'Geo-response',
+            template: 'index.hbs',
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
+};
